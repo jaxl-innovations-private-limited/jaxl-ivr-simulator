4ab2e7908c4604a012ab407277703d09bb4011c0
ScripticArnav <arnav.1807prof@gmail.com>
2024-06-19 16:29:32 +0530

    commit

--- tests/test_anyTimeJoke.py
+++ tests/test_anyTimeJoke.py
@@ -1,39 +1,5 @@
 """
 Copyright (c) 2010-present by Jaxl Innovations Private Limited.
-
-All rights reserved.
-
-Redistribution and use in source and binary forms,
-with or without modification, is strictly prohibited.
-"""
-
-# from webhooks.anyTimeJoke import JaxlIVRAnytimejokeWebhook
-# from jaxl.ivr.frontend.base import BaseJaxlIVRWebhookTestCase
-
-
-# class TestJaxlIVRAnytimejokeWebhook(BaseJaxlIVRWebhookTestCase):
-#     """Test cases for JaxlIVRAnytimejokeWebhook IVR."""
-
-#     webhook_klass = JaxlIVRAnytimejokeWebhook
-
-#     def setUp(self) -> None:
-#         super().setUp()
-
-#         # Start the call to webhook during setup
-#         self.start_call()
-
-#     def test_main_menu(self) -> None:
-#         """Tests main menu of your IVR i.e. response returned by start_call operation.
-
-#         Response of start_call is available as `self.start_call_response`.
-#         """
-#         # Complete me
-#         self.assertEqual(1, 1)
-
-
-        
-"""
-Copyright (c) 2010-present by Jaxl Innovations Private Limited.
 All rights reserved.
 Redistribution and use in source and binary forms,
 with or without modification, is strictly prohibited.
