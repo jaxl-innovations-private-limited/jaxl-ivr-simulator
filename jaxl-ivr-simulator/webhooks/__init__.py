2fc5a051e48afa16ee5c7f06e75c82a020e30288
Yash Gupta <98685453+yashgupta749@users.noreply.github.com>
2024-06-20 16:38:00 +0530

    Delete jaxl-ivr-simulator directory

--- jaxl-ivr-simulator/webhooks/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Copyright (c) 2010-present by Jaxl Innovations Private Limited.
-
-All rights reserved.
-
-Redistribution and use in source and binary forms,
-with or without modification, is strictly prohibited.
-"""
